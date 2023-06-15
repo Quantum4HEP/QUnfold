8b35d346ea1f62ceccabe79e9db323bffae94765
Gianluca Bianco <gianluca.bianco3@studio.unibo.it>
2023-06-15 17:24:57 +0200

    Unfolding bash script update.

--- studies/RooUnfold/unfolding.sh
+++ studies/RooUnfold/unfolding.sh
@@ -8,7 +8,23 @@
 # Copyright:  (c) 2023 Gianluca Bianco under the MIT license.
 
 # Variables
-distr="breit-wigner"
+distr="normal breit-wigner double-peaked"
+only_one_distr="" # intialize to unfold only one distribution
 
 # Run the unfolding
-./RooUnfold/unfolding.py --distr=${distr}
+if [ -n "${only_one_distr}" ] ; then
+    echo "Unfolding only the ${only_one_distr} distribution:"
+    echo ""
+    ./RooUnfold/unfolding.py --distr="${only_one_distr}"
+else
+    echo "Unfolding all the distributions"
+    echo ""
+    for distr_ in ${distr}
+    do
+        echo "Unfolding the ${distr_} distribution:"
+        echo ""
+        ./RooUnfold/unfolding.py --distr="${distr_}"
+        echo ""
+    done
+fi
+
