6b07a24aae2d2192b609c7ef4c1a9f1aaaff096f
Gianluca Bianco <gianluca.bianco3@studio.unibo.it>
2023-06-18 22:56:29 +0200

    Added script to automatic fill requirements.txt: - It is integrated with the root tox.ini file.

---
+++ scripts/fill_requirements.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ---------------------- Metadata ----------------------
+#
+# File name:  fill_requirements.sh
+# Author:     Gianluca Bianco (biancogianluca9@gmail.com)
+# Date:       2023-06-18
+# Copyright:  (c) 2023 Gianluca Bianco under the MIT license.
+
+pipreqs src --savepath requirements.txt 2> /dev/null
