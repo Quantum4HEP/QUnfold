ed7748e9ed2f4d456a63e01ef380ac168442695a
SimoneGasperini <simone.gasperini4@unibo.it>
2023-07-26 17:41:17 +0200

    Add method to run quantum unfolding algorithm by simulated annealing

--- src/QUnfold/core/QUnfoldQUBO.py
+++ src/QUnfold/core/QUnfoldQUBO.py
@@ -52,3 +52,12 @@ class QUnfoldQUBO:
                 hamiltonian += B[i, j] * x[i] * x[j]
         model = hamiltonian.compile()
         return labels, model
+
+    def run_simulated_annealing(self, response, data, lam=0.1, num_reads=100):
+        labels, model = self._get_pyqubo_model(response, data, lam)
+        sampler = SimulatedAnnealingSampler()
+        sampleset = sampler.sample(model.to_bqm(), num_reads=num_reads)
+        decoded_sampleset = self._model.decode_sampleset(sampleset)
+        best_sample = min(decoded_sampleset, key=lambda s: s.energy)
+        solution = np.array([best_sample.subh[label] for label in labels])
+        return solution.astype(int)
