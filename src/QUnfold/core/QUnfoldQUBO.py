d2c935a5f4fffe504c18b84e097ee6d9fbaf2ed4
SimoneGasperini <simone.gasperini4@unibo.it>
2023-07-26 17:34:20 +0200

    Implement pyqubo model for quantum unfolding algorithm

--- src/QUnfold/core/QUnfoldQUBO.py
+++ src/QUnfold/core/QUnfoldQUBO.py
@@ -8,34 +8,47 @@
 # Date:       2023-06-16
 # Copyright:  (c) 2023 Gianluca Bianco under the MIT license.
 
+import numpy as np
+from pyqubo import LogEncInteger
+from dwave.samplers import SimulatedAnnealingSampler
+
 
 class QUnfoldQUBO:
-    """
-    Represents the QUnfold class which uses QUBO approach to solve the unfolding problem.
-    """
 
-    def __init__(self, response, meas_bin_contents, meas_bin_edges):
-        """
-        Construct a QUnfoldQUBO object with measured histogram and response matrix.
-
-        Args:
-            response (np.ndarray): 2-dim NumPy array which represents the response matrix.
-            meas_bin_contents (np.ndarray): 1-dim NumPy array which represents the measured histogram bin contents.
-            meas_bin_edges (np.ndarray): 1-dim NumPy array which represents the measured histogram bin edges.
-        """
-
-        # Initialize variables
-        self.response = response
-        self.measured_bin_contents = meas_bin_contents
-        self.measured_bin_edges = meas_bin_edges
-
-        # Transform the inputs into binary
-        # ...
-
-    def unfold(self):
-        """
-        Method used to perform the unfolding using QUBO approach.
-        """
-
-        # Todo...
+    def __init__(self):
         pass
+
+    @staticmethod
+    def _get_laplacian(dim):
+        diag = np.ones(dim, dtype=int) * -2
+        ones = np.ones(dim-1, dtype=int)
+        return np.diag(diag) + np.diag(ones, k=1) + np.diag(ones, k=-1)
+
+    @staticmethod
+    def _compute_linear(R, d):
+        return -2. * np.matmul(np.transpose(R), d)
+
+    @staticmethod
+    def _compute_quadratic(R, G, lam):
+        return np.matmul(np.transpose(R), R) + lam * np.matmul(np.transpose(G), G)
+
+    def _get_pyqubo_model(self, response, data, lam):
+        num_bins = len(data)
+        num_entries = int(sum(data))
+        labels = [f'x{i}' for i in range(num_bins)]
+        # variables binary encoding
+        x = [LogEncInteger(label=label, value_range=(0, num_entries))
+             for label in labels]
+        hamiltonian = 0
+        # linear terms
+        a = self._compute_linear(R=response, d=data)
+        for i in range(len(x)):
+            hamiltonian += a[i] * x[i]
+        # quadratic terms
+        G = self._get_laplacian(dim=num_bins)
+        B = self._compute_quadratic(R=response, G=G, lam=lam)
+        for i in range(len(x)):
+            for j in range(len(x)):
+                hamiltonian += B[i, j] * x[i] * x[j]
+        model = hamiltonian.compile()
+        return labels, model
