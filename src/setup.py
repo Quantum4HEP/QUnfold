0925c6b6e40b555ea79a03cf9527a17cd57f57d5
SimoneGasperini <simone.gasperini4@unibo.it>
2023-11-06 17:57:41 +0100

    Add setup.py file for Python package

---
+++ src/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+if __name__ == "__main__":
+    setup(
+        name="QUnfold",
+        packages=find_packages(include=["QUnfold"]),
+        version="0.0.2",
+        author=["Gianluca Bianco", "Simone Gasperini"],
+        author_email=["biancogianluca9@gmail.com", "simone.gasperini4@unibo.it"],
+        url="https://github.com/JustWhit3/QUnfold/tree/main/src/QUnfold",
+        install_requires=[
+            "dwave-ocean-sdk",
+            "pyqubo",
+            "matplotlib",
+            "scipy",
+        ],
+    )
