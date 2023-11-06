7174071adbd9aa070c29444925f831e7402776d5
SimoneGasperini <simone.gasperini4@unibo.it>
2023-11-06 18:08:53 +0100

    Minor fix to QUnfold setup.py file

--- src/setup.py
+++ src/setup.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
         name="QUnfold",
         packages=find_packages(include=["QUnfold"]),
         version="0.0.2",
-        author=["Gianluca Bianco", "Simone Gasperini"],
-        author_email=["biancogianluca9@gmail.com", "simone.gasperini4@unibo.it"],
+        author="Gianluca Bianco, Simone Gasperini",
+        author_email="biancogianluca9@gmail.com, simone.gasperini4@unibo.it",
         url="https://github.com/JustWhit3/QUnfold/tree/main/src/QUnfold",
+        python_requires=">=3.8",
         install_requires=[
             "dwave-ocean-sdk",
             "pyqubo",
