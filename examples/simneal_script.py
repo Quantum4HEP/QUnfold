4962045e41bb7a77e3e67df8271f5eb9b27b91e5
JustWhit3 <biancogianluca9@gmail.com>
2023-12-08 17:43:10 +0100

    Removed useless lines from example code

--- examples/simneal_script.py
+++ examples/simneal_script.py
@@ -29,13 +29,11 @@ std_smear = 0.5
 
 # Generate and normalize response matrix
 mc_data = np.random.normal(loc=mean, scale=std, size=num_entries)
-print(mc_data)
 reco_data = mc_data + np.random.normal(
     loc=mean_smear, scale=std_smear, size=num_entries
 )
 response, _, _ = np.histogram2d(reco_data, mc_data, bins=bins)
 mc_truth, _ = np.histogram(mc_data, bins=bins)
-print(mc_truth)
 response = normalize_response(response, mc_truth)
 
 # Generate random true data
@@ -59,8 +57,5 @@ unfolded = unfolder.solve_simulated_annealing(num_reads=100, seed=seed)
 plotter = QUnfoldPlotter(
     response=response, measured=measured, truth=true, unfolded=unfolded, binning=bins
 )
-plotter.plotResponse()
-plotter.plot()
-
 plotter.saveResponse("examples/simneal_response.png")
 plotter.savePlot("examples/simneal_result.png", method="SA")
