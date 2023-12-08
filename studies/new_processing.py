e3fc7bd67c978238e3a99f363fbd700ae72dea76
DrWatt <marco.lorusso96@gmail.com>
2023-12-08 18:45:28 +0100

    Isolated and partially generalized electron-muon filter and generation with lepton etas

--- studies/new_processing.py
+++ studies/new_processing.py
@@ -2,14 +2,13 @@ import uproot
 import awkward as ak
 import numpy as np
 
-
 def get_info_particle_level(particle_info, list_particleIDs,list_partvars):
-    lepton_mask = ak.Array(np.zeros(len(particle_info),dtype=bool))
+    particle_mask = ak.Array(np.zeros(len(particle_info),dtype=bool))
     for ids in list_particleIDs:
-        lepton_mask = lepton_mask | (abs(particle_info["Particle.PID"]) == ids)
+        particle_mask = particle_mask | (abs(particle_info["Particle.PID"]) == ids)
     outvars = {}
     for partvars in list_partvars:
-        temp = np.sort(ak.mask(particle_info[partvars], lepton_mask), axis=-1)
+        temp = np.sort(ak.mask(particle_info[partvars], particle_mask), axis=-1)
         outvars[partvars] = [temp[:, 1], temp[:, 0]]
     return outvars
 
@@ -17,6 +16,7 @@ def  get_info_reco_level(reco_info, list_recovars):
     outvars = {}
     for recovars in list_recovars:
         outvars[recovars] = reco_info[recovars]
+
     return outvars
 
 
@@ -42,11 +42,9 @@ def tree_reader(reco_file, particle_file, reco_var_list, part_val_dict):
     return [events_idx,reco_info,all_part_info]
 
 
-def hist_resp_builder(reco_lead,reco_sublead,part_lead,part_sublead,do_response = False):
+def hist_resp_builder(reco_lead,reco_sublead,part_lead,part_sublead,num_bins,lead_binning,sublead_binning,do_response = False):
 
-    num_bins = 19
-    lead_binning = np.linspace(50, 200, num_bins + 1)
-    sublead_binning = np.linspace(30, 120, num_bins + 1)
+    
     mask_lead = reco_lead != None
     mask_sublead = reco_sublead != None
     # print(reco_lead)
@@ -72,6 +70,30 @@ def hist_resp_builder(reco_lead,reco_sublead,part_lead,part_sublead,do_response 
         return hist_reco_lead, hist_part_lead, hist_reco_sublead, hist_part_sublead
 
 
+def lepton_reco_filter(events_idx,reco_var_e,reco_var_mu,e_size,mu_size):
+    mask_ee = (e_size == 2) & (mu_size == 0)
+    events_ee_idx = events_idx[mask_ee]
+    reco_pt_ee = np.sort(reco_var_e[mask_ee], axis=-1)
+    
+    mask_mumu = (e_size == 0) & (mu_size == 2)
+    events_mumu_idx = events_idx[mask_mumu]
+    reco_pt_mumu = np.sort(reco_var_mu[mask_mumu], axis=-1)
+    
+    mask_emu = (e_size == 1) & (mu_size == 1)
+    events_emu_idx = events_idx[mask_emu]
+    reco_pt_emu = np.sort(np.hstack((reco_var_e[mask_emu], reco_var_mu[mask_emu])), axis=-1)
+
+
+    temp = np.concatenate((reco_pt_ee, reco_pt_mumu, reco_pt_emu))
+    concat_pt = np.array([[None, None]] * len(events_idx))
+
+    concat_idx = np.concatenate((events_ee_idx, events_mumu_idx, events_emu_idx))
+
+    concat_pt[mask_ee | mask_mumu | mask_emu] = temp[np.argsort(concat_idx)]
+
+
+    return concat_pt[:, 1], concat_pt[:, 0]
+
 def process(reco_tree_path, part_tree_path, list_recovars, dict_partvars, do_response = False):
     events_idx, reco_info, particle_info = tree_reader(reco_tree_path, part_tree_path, list_recovars, dict_partvars)
 
@@ -81,28 +103,35 @@ def process(reco_tree_path, part_tree_path, list_recovars, dict_partvars, do_res
     electron_pt = reco_info["Electron.PT"]
     muon_pt = reco_info["Muon.PT"]
 
-    #emu_part_vars = get_info_particle_level(particle_info,[11,13],["Particle.PID", "Particle.PT", "Particle.Eta","Particle.Phi","Particle.E"])
-    # part_level_vars = get_info_particle_level(particle_info,[int(num) for num in list(dict_partvars.keys())[0].split(',')],["Particle.PID", "Particle.PT", "Particle.Eta","Particle.Phi","Particle.E"])
-    # part_level_vars_jet = get_info_particle_level(particle_info,[5],["Particle.PID",  "Particle.Eta","Particle.Phi"])
-    # print(part_level_vars["Particle.PT"])
+    electron_Eta = reco_info["Electron.Eta"]
+    muon_Eta = reco_info["Muon.Eta"]
 
-    mask_ee = (electron_size == 2) & (muon_size == 0)
-    events_ee_idx = events_idx[mask_ee]
-    reco_pt_ee = np.sort(electron_pt[mask_ee], axis=-1)
+    reco_pT_lep1, reco_pT_lep2 = lepton_reco_filter(events_idx, electron_pt, muon_pt, electron_size, muon_size)
+    particle_pT_lep1, particle_pT_lep2 = particle_info["11,13"]["Particle.PT"]
 
-    mask_mumu = (electron_size == 0) & (muon_size == 2)
-    events_mumu_idx = events_idx[mask_mumu]
-    reco_pt_mumu = np.sort(muon_pt[mask_mumu], axis=-1)
+    reco_Eta_lep1, reco_Eta_lep2 = lepton_reco_filter(events_idx, electron_Eta, muon_Eta, electron_size, muon_size)
+    particle_Eta_lep1, particle_Eta_lep2 = particle_info["11,13"]["Particle.Eta"]
 
-    mask_emu = (electron_size == 1) & (muon_size == 1)
-    events_emu_idx = events_idx[mask_emu]
-    reco_pt_emu = np.sort(np.hstack((electron_pt[mask_emu], muon_pt[mask_emu])), axis=-1)
+    # print(particle_Eta_lep1)
+    num_bins = 19
+    lead_binning = np.linspace(50, 200, num_bins + 1)
+    sublead_binning = np.linspace(30, 120, num_bins + 1)
 
-    temp = np.concatenate((reco_pt_ee, reco_pt_mumu, reco_pt_emu))
-    concat_pt = np.array([[None, None]] * num_events)
-    concat_idx = np.concatenate((events_ee_idx, events_mumu_idx, events_emu_idx))
-    concat_pt[mask_ee | mask_mumu | mask_emu] = temp[np.argsort(concat_idx)]
-    reco_pT_lep1, reco_pT_lep2 = concat_pt[:, 1], concat_pt[:, 0]
+    num_bins2 = 19
+    lead_binning2 = np.linspace(0, 3, num_bins2 + 1)
+    sublead_binning2 = np.linspace(0, 3, num_bins2 + 1)
+
+    processed = []
+
+    processed.append(hist_resp_builder(reco_pT_lep1, reco_pT_lep2, particle_pT_lep1, particle_pT_lep2,num_bins,lead_binning,sublead_binning,do_response))
+    processed.append(hist_resp_builder(reco_Eta_lep1, reco_Eta_lep2, particle_Eta_lep1, particle_Eta_lep2,num_bins2,lead_binning2,sublead_binning2,do_response))
+
+
+
+    
+    return processed
+    
+
 
     # reco_pT_lep1______ = np.loadtxt("reco_pT_lep1.txt", delimiter=",")
     # reco_pT_lep2______ = np.loadtxt("reco_pT_lep2.txt", delimiter=",")
@@ -113,8 +142,6 @@ def process(reco_tree_path, part_tree_path, list_recovars, dict_partvars, do_res
     # particle_pT_lep2______ = np.loadtxt("particle_pT_lep2.txt", delimiter=",")
     # assert np.all(particle_pT_lep1 == particle_pT_lep1______)
     # assert np.all(particle_pT_lep2 == particle_pT_lep2______)
-    particle_pT_lep1, particle_pT_lep2 = particle_info["11,13"]["Particle.PT"]
-    return hist_resp_builder(reco_pT_lep1, reco_pT_lep2, particle_pT_lep1, particle_pT_lep2,do_response)
 
     # resp_pt_lep1______ = np.loadtxt("resp_pt_lep1.txt")
     # resp_pt_lep2______ = np.loadtxt("resp_pt_lep2.txt")
@@ -124,13 +151,13 @@ def process(reco_tree_path, part_tree_path, list_recovars, dict_partvars, do_res
 
 if __name__ == '__main__':
 
-    list_recovars = ["Electron_size", "Muon_size", "Electron.PT", "Muon.PT","Electron.Eta"] 
+    list_recovars = ["Electron_size", "Muon_size", "Electron.PT", "Muon.PT","Electron.Eta","Muon.Eta"] 
     # list_partvars = ["Particle.PID", "Particle.PT", "Particle.Eta","Particle.Phi","Particle.E"]
     dict_partvars = {"11,13": ["Particle.PT", "Particle.Eta","Particle.Phi","Particle.E"],"5":["Particle.Eta","Particle.Phi"]}
-    reco_tree_path = "tag_1_delphes_events_small.root"
-    part_tree_path = "unweighted_events_small.root"
+    reco_tree_path = "../../tag_1_delphes_events_small.root"
+    part_tree_path = "../../unweighted_events_small.root"
     outp = process(reco_tree_path, part_tree_path, list_recovars, dict_partvars, do_response = False)
-    # print(outp[0],outp[1],outp[2],outp[3])
+    print(outp[1][0],outp[1][1],outp[1][2],outp[1][3])
     outp = process(reco_tree_path, part_tree_path, list_recovars, dict_partvars, do_response = True)
-    # print(outp[0],outp[1],outp[2],outp[3])
+    print(outp[1][0],outp[1][1],outp[1][2],outp[1][3])
     print("ho fatto la response")
