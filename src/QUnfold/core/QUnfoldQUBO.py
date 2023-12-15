3e80a81274292f869534320c0b8458367837403c
SimoneGasperini <simone.gasperini4@unibo.it>
2023-12-15 21:19:41 +0100

    Implement several pre- and post-processing fixes to the QUnfoldQUBO class

--- src/QUnfold/core/QUnfoldQUBO.py
+++ src/QUnfold/core/QUnfoldQUBO.py
@@ -66,28 +66,26 @@ class QUnfoldQUBO:
         D += np.diag(diag1, k=1) + np.diag(diag1, k=-1)
         return D
 
-    def _define_variables(self, opt):
+    def _get_expected_num_entries(self):
+        """
+        TODO: docstring
+        """
+        efficiencies = np.sum(self.R, axis=0)
+        num_entries = int(sum(self.d / efficiencies))
+        return num_entries
+
+    def _define_variables(self):
         """
         Define the list of variables for the QUBO problem.
-
-        Parameters:
-            opt (bool): enables optimal range for the integer variables.
 
         Returns:
             list: encoded integer variables.
         """
-        variables = []
-        if opt:
-            for i, mean in enumerate(self.d):
-                error = 5 * mean**0.5
-                lower = 0 if mean - error < 0 else int(np.floor(mean - error))
-                upper = 1 if mean == 0 else int(np.ceil(mean + error))
-                variables.append(LogEncInteger(f"x{i}", value_range=(lower, upper)))
-        else:
-            variables = [
-                LogEncInteger(f"x{i}", value_range=(0, int(sum(self.d))))
-                for i in range(len(self.d))
-            ]
+        num_entries = self._get_expected_num_entries()
+        variables = [
+            LogEncInteger(f"x{i}", value_range=(0, num_entries))
+            for i in range(len(self.d))
+        ]
         return variables
 
     def _define_hamiltonian(self, x):
@@ -102,13 +100,9 @@ class QUnfoldQUBO:
         """
         hamiltonian = 0
         dim = len(x)
-
-        # Add linear terms
         a = -2 * (self.R.T @ self.d)
         for i in range(dim):
             hamiltonian += a[i] * x[i]
-
-        # Add quadratic terms:
         G = self._get_laplacian(dim)
         B = (self.R.T @ self.R) + self.lam * (G.T @ G)
         for i in range(dim):
@@ -116,34 +110,39 @@ class QUnfoldQUBO:
                 hamiltonian += B[i, j] * x[i] * x[j]
         return hamiltonian
 
-    def initialize_qubo_model(self, optimize_vars_range=False):
+    def _post_process_sampleset(self, sampleset):
+        """
+        TODO: docstring
+        """
+        median_energy = np.median(sampleset.record.energy)
+        filtered_sampleset = sampleset.filter(pred=lambda s: s.energy < median_energy)
+        results = np.array(
+            [
+                [sample.subh[label] for label in self.labels]
+                for sample in self.model.decode_sampleset(filtered_sampleset)
+            ]
+        )
+        solution = np.round(np.mean(results, axis=0))
+        error = np.std(results, axis=0)
+        return solution, error
+
+    def initialize_qubo_model(self):
         """
         Initialize QUBO model and BQM instance for the unfolding problem.
-
-        Parameters:
-            optimize_vars_range (bool, optional): enables optimal range for the integer variables (default is False).
         """
-        x = self._define_variables(opt=optimize_vars_range)
+        x = self._define_variables()
         h = self._define_hamiltonian(x)
         self.labels = [x[i].label for i in range(len(x))]
         self.model = h.compile()
         self.bqm = self.model.to_bqm()
 
-    def solve_simulated_annealing(self, num_reads=1, seed=None):
+    def solve_simulated_annealing(self, num_reads, seed=None):
         """
         TODO: docstring
         """
         sampler = SimulatedAnnealingSampler()
-        sample_set = sampler.sample(self.bqm, num_reads=num_reads, seed=seed)
-        solution_set = np.array(
-            [
-                [sample.subh[label] for label in self.labels]
-                for sample in self.model.decode_sampleset(sample_set)
-            ]
-        )
-        solution = np.round(np.mean(solution_set, axis=0))
-        cov_matrix = np.cov(solution_set, rowvar=False, ddof=1)
-        error = np.sqrt(np.diagonal(cov_matrix) / num_reads)
+        sampleset = sampler.sample(self.bqm, num_reads=num_reads, seed=seed)
+        solution, error = self._post_process_sampleset(sampleset=sampleset)
         return solution, error
 
     def solve_quantum_annealing(self, num_reads):
@@ -154,17 +153,12 @@ class QUnfoldQUBO:
 
     def solve_hybrid_sampler(self):
         """
-        Solve the unfolding QUBO problem using the Leap Hybrid sampler.
-
-        Returns:
-            numpy.ndarray: unfolded histogram.
+        TODO: docstring
         """
-        # TODO: fix solution selection and error computation
         sampler = LeapHybridSampler()
         sampleset = sampler.sample(self.bqm)
-        decoded_sampleset = self.model.decode_sampleset(sampleset)
-        best_sample = min(decoded_sampleset, key=lambda s: s.energy)
-        return np.array([best_sample.subh[label] for label in self.labels])
+        solution, error = self._post_process_sampleset(sampleset=sampleset)
+        return solution, error
 
     def compute_energy(self, x):
         """
@@ -176,8 +170,8 @@ class QUnfoldQUBO:
         Returns:
             float: energy for the given input.
         """
-        # TODO: fix for the case 'optimize_vars_range=True'
-        num_bits = int(np.floor(np.log2(sum(self.d))))
+        num_entries = self._get_expected_num_entries()
+        num_bits = int(np.ceil(np.log2(num_entries)))
         x_binary = {}
         for i, entry in enumerate(x):
             bitstr = np.binary_repr(int(entry), width=num_bits)
