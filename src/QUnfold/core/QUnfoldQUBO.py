Fix error computation by using the standard deviation of the mean

src/QUnfold/core/QUnfoldQUBO.py
@@ -126,7 +126,7 @@ class QUnfoldQUBO:
             ]
         )
         solution = np.round(np.mean(results, axis=0))
-        error = np.std(results, axis=0)
+        error = np.std(results, axis=0) / np.sqrt(len(solution))
         return solution, error
 
     def initialize_qubo_model(self):