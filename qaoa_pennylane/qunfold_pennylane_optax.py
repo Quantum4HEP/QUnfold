1f0d29d7f159a438073ddd00b610e0ffb34851b4
SimoneGasperini <simone.gasperini4@unibo.it>
2024-04-18 12:35:29 +0200

    Fix QUBO cost Hamiltonian for QAOA implementation in Pennylane

--- qaoa_pennylane/qunfold_pennylane_optax.py
+++ qaoa_pennylane/qunfold_pennylane_optax.py
@@ -6,38 +6,52 @@ import optax
 
 a_vector = np.loadtxt("a_vector.txt")
 B_matrix = np.loadtxt("B_matrix.txt")
+solution_bitstring = np.loadtxt("min_energy_bitstring.txt").astype(int)
+solution_histogram = np.loadtxt("min_energy_solution.txt").astype(int)
 upper_bounds = np.loadtxt("upper_bounds.txt")
-print("\nUpper bounds =")
-print(upper_bounds)
+
+print("Upper bounds =", upper_bounds)
 
 qubits_per_var = [int(np.log2(ub + 1)) for ub in upper_bounds]
-print("\nQubits per var =")
-print(qubits_per_var)
+print("Qubits per var =", qubits_per_var)
 
 num_wires = sum(qubits_per_var)
 dev = qml.device("default.qubit", wires=num_wires)
 p = depth = 1
 
-def qubo_hamitonian(a, B):
+
+def qubo_hamitonian(a, B, offset=False):
     coeffs_linear = []
     obs_linear = []
     for i in range(num_wires):
-        if a[i] != 0:
-            coeffs_linear.append(a[i])
-            obs_linear.append(qml.PauliZ(i))
+        coeff = a[i] / 2
+        if offset:
+            coeffs_linear.append(coeff)
+            obs_linear.append(qml.I(i))
+        coeffs_linear.append(-coeff)
+        obs_linear.append(qml.PauliZ(i))
     H_linear = qml.Hamiltonian(coeffs=coeffs_linear, observables=obs_linear)
     coeffs_quadratic = []
     obs_quadratic = []
-    for i in range(num_wires-1):
-        for j in range(i+1, num_wires):
-            if B[i, j] != 0:
-                coeffs_quadratic.append(B[i, j])
-                obs_quadratic.append(qml.PauliZ(i) @ qml.PauliZ(j))
+    for i in range(num_wires - 1):
+        for j in range(i + 1, num_wires):
+            coeff = B[i, j] / 4
+            if offset:
+                coeffs_quadratic.append(coeff)
+                obs_quadratic.append(qml.I(i) @ qml.I(j))
+            coeffs_quadratic.append(-coeff)
+            coeffs_quadratic.append(-coeff)
+            coeffs_quadratic.append(coeff)
+            obs_quadratic.append(qml.PauliZ(i))
+            obs_quadratic.append(qml.PauliZ(j))
+            obs_quadratic.append(qml.PauliZ(i) @ qml.PauliZ(j))
     H_quadratic = qml.Hamiltonian(coeffs=coeffs_quadratic, observables=obs_quadratic)
     H_qubo = H_linear + H_quadratic
-    return H_qubo
+    return H_qubo.simplify()
+
 
 H_qubo = qubo_hamitonian(a=a_vector, B=B_matrix)
+
 
 def mixer_hamiltonian():
     coeffs = jnp.ones(len(dev.wires))
@@ -45,17 +59,21 @@ def mixer_hamiltonian():
     H_mixer = qml.Hamiltonian(coeffs=coeffs, observables=obs)
     return H_mixer
 
+
 H_mixer = mixer_hamiltonian()
+
 
 def qaoa_layer(gamma, alpha):
     qml.qaoa.cost_layer(gamma=gamma, hamiltonian=H_qubo)
     qml.qaoa.mixer_layer(alpha=alpha, hamiltonian=H_mixer)
 
+
 def var_circuit(params):
     for wire in dev.wires:
         qml.Hadamard(wires=wire)
-    gamma, alpha = params
-    qml.layer(qaoa_layer, depth, gamma, alpha)
+    gammas, alphas = params
+    qml.layer(qaoa_layer, depth, gammas, alphas)
+
 
 @jax.jit
 @qml.qnode(device=dev)
@@ -63,7 +81,9 @@ def cost_function(params):
     var_circuit(params=params)
     return qml.expval(H_qubo)
 
+
 opt = optax.adam(learning_rate=0.3)
+
 
 @jax.jit
 def update_step(_, args):
@@ -73,6 +93,7 @@ def update_step(_, args):
     params = optax.apply_updates(params, updates)
     return params, opt_state
 
+
 @jax.jit
 def run_optimization(params, iterations):
     opt_state = opt.init(params)
@@ -80,31 +101,40 @@ def run_optimization(params, iterations):
     params, opt_state = jax.lax.fori_loop(0, iterations, update_step, args)
     return params
 
-init_params = jnp.array([[0.5]*depth, [0.5]*depth])
-print("\nCompiling the optimization routine...")
+
+init_params = jnp.array([[0.5] * depth, [0.5] * depth])
+print("Compiling the optimization routine... ", end="")
 run_optimization(params=init_params, iterations=1)
-print("Done")
+print("Done!")
 
 iterations = 100
 fin_params = run_optimization(params=init_params, iterations=iterations)
-print("\nFinal params =")
+print("Final params =")
 print(fin_params)
 
 dev = qml.device("default.qubit", wires=num_wires, shots=1)
+
+
 @qml.qnode(device=dev)
 def sample_circuit(params):
     var_circuit(params)
     return qml.sample()
 
-sample = sample_circuit(params=fin_params)
-bitlist = [str(bit) for bit in sample]
+
+bitstring = sample_circuit(params=fin_params)
+
+print("Output bitstring =", bitstring)
+print("Target bitstring =", solution_bitstring)
+
+bitlist = [str(bit) for bit in bitstring]
 histogram = []
 start = 0
 end = 0
 for shift in qubits_per_var:
     end += shift
-    bitstring = "".join(bitlist[start:end])
-    histogram.append(int(bitstring, base=2))
+    bitstr = "".join(bitlist[start:end])
+    histogram.append(int(bitstr[::-1], base=2))
     start += shift
-print("\nOutput histogram =")
-print(histogram)
+
+print("Output histogram =", np.array(histogram))
+print("Target histogram =", solution_histogram)
