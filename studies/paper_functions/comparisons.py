06b5331196407faa3185f7044fbd3d5b031ecc1e
JustWhit3 <biancogianluca9@gmail.com>
2024-07-01 22:08:32 +0200

    Minor updates and code reorganization in the paper studies

--- studies/paper_functions/comparisons.py
+++ studies/paper_functions/comparisons.py
@@ -4,11 +4,11 @@ import numpy as np
 import pylab as plt
 from QUnfold import QUnfoldQUBO
 from QUnfold.utility import (
-    TH1_to_array,
-    TH2_to_array,
+    TH1_to_numpy,
+    TH2_to_numpy,
     normalize_response,
     compute_chi2,
-    TMatrix_to_array,
+    TMatrix_to_numpy,
 )
 
 # RooUnfold settings
@@ -18,9 +18,68 @@ if not loaded_RooUnfold == 0:
     sys.exit(0)
 
 
+def run_RooUnfold(method, m_response, h_measured, truth, num_toys=1):
+    # Setup unfolder
+    unfolder = None
+    if method == "IBU":
+        unfolder = ROOT.RooUnfoldBayes("IBU", "Iterative Bayesian")
+        unfolder.SetIterations(4)
+        unfolder.SetVerbose(0)
+        unfolder.SetSmoothing(0)
+        unfolder.SetResponse(m_response)
+        unfolder.SetMeasured(h_measured)
+    elif method == "MI":
+        unfolder = ROOT.RooUnfoldInvert("MI", "Matrix Inversion")
+        unfolder.SetVerbose(0)
+        unfolder.SetResponse(m_response)
+        unfolder.SetMeasured(h_measured)
+    unfolded_histo = None
+
+    # Compute solution
+    if num_toys == 1:
+        unfolded_histo = unfolder.Hunfold(unfolder.kErrors)
+    elif num_toys > 1:
+        unfolder.SetNToys(num_toys)
+        unfolded_histo = unfolder.Hunfold(unfolder.kCovToys)
+    unfolded = TH1_to_numpy(unfolded_histo)
+    start, stop = 1, unfolded_histo.GetNbinsX() + 1
+    error = np.array([unfolded_histo.GetBinError(i) for i in range(start, stop)])
+
+    # Compute chi2
+    chi2 = None
+    if num_toys == 1:
+        chi2 = compute_chi2(unfolded, truth)
+    elif num_toys > 1:
+        cov = unfolder.Eunfold(unfolder.kCovToys)
+        chi2 = compute_chi2(unfolded, truth, TMatrix_to_numpy(cov)[1:-1, 1:-1])
+
+    return unfolded, error, chi2
+
+
+def run_QUnfold(method, unfolder, response, measured, truth, num_reads, num_toys=1):
+    # Initialize the unfolder
+    if method == "SimNeal":
+        unfolded, error, cov = unfolder.solve_simulated_annealing(
+            num_reads=num_reads, num_toys=num_toys
+        )
+    elif method == "Hyb":
+        unfolded, error, cov = unfolder.solve_hybrid_sampler(num_toys=num_toys)
+
+    # Return solution without overflow bins
+    unfolded = unfolded[1:-1]
+    error = error[1:-1]
+    cov = cov[1:-1, 1:-1]
+
+    chi2 = compute_chi2(unfolded, truth, cov)
+
+    return unfolded, error, chi2
+
+
 def make_plots(
-    SA_info, HYB_info, IBU_info, MI_info, truth, measured, binning, var, quantum
+    SA_info, HYB_info, IBU_info, MI_info, truth, measured, binning, var, hybrid
 ):
+    measured = measured[1:-1]
+
     # Divide into subplots
     fig = plt.figure(figsize=(8.0, 6.4))
     gs = fig.add_gridspec(2, 1, height_ratios=[3, 1], hspace=0)
@@ -123,7 +182,7 @@ def make_plots(
     )
 
     # Plot HYB
-    if quantum == True:
+    if hybrid == True:
         HYB_chi2 = HYB_info["chi2"]
         label = r"{} ($\chi^2 = {:.2f}$)".format(r"$\mathtt{QUnfold}$ (HYB)", HYB_chi2)
         ax1.errorbar(
@@ -188,12 +247,13 @@ def make_comparisons(reco, particle):
         "pT_lep2",
         "m_l1l2",
         "m_b1b2",
-        # "DR_b1b2",
-        # "eta_lep1",
-        # "eta_lep2",
+        "DR_b1b2",
+        "eta_lep1",
+        "eta_lep2",
     ]
-    num_toys = 50
-    quantum = False
+    num_toys = 1000
+    num_reads = 300
+    hybrid = False
 
     # RUnning over variables
     for var in variables:
@@ -205,22 +265,23 @@ def make_comparisons(reco, particle):
         h_truth = particle.Get(f"particle_{var}")
         h_mc_measured = reco.Get(f"mc_{var}")
         h_mc_truth = particle.Get(f"mc_particle_{var}")
-
-        # Prepare unfolding input
-        measured = TH1_to_array(h_measured, overflow=True)
-        truth = TH1_to_array(h_truth, overflow=True)
-        response = normalize_response(
-            TH2_to_array(m_response, overflow=True),
-            TH1_to_array(h_mc_truth, overflow=True),
-        )
         binning = [
             reco.Get(var).GetXaxis().GetBinLowEdge(bin)
             for bin in range(1, reco.Get(var).GetNbinsX() + 2)
         ]
 
+        ################################### QUnfold ###################################
+
+        # Prepare unfolding input
+        measured = TH1_to_numpy(h_measured, overflow=True)
+        truth = TH1_to_numpy(h_truth)
+        response = normalize_response(
+            TH2_to_numpy(m_response, overflow=True),
+            TH1_to_numpy(h_mc_truth, overflow=True),
+        )
+
         # Unfold with QUnfold (basic settings)
         lam = 0.0
-        num_reads = 100  # 1000
         if var == "pT_lep1":
             lam = 0.0001  # 0.00001
         elif var == "pT_lep2":
@@ -236,80 +297,36 @@ def make_comparisons(reco, particle):
         elif var == "eta_lep2":
             lam = 0.00001
         unfolder = QUnfoldQUBO(measured=measured, response=response, lam=lam)
+        unfolder.initialize_qubo_model()
 
         # Simulated annealing
-        unfolded_SA, error_SA, cov_SA, _ = unfolder.solve_simulated_annealing(
-            num_reads=num_reads, num_toys=num_toys
+        unfolded_SA, error_SA, chi2_SA = run_QUnfold(
+            "SimNeal", unfolder, response, measured, truth, num_reads, num_toys
         )
-        chi2_SA = None
-        if cov_SA is None:
-            chi2_SA = compute_chi2(unfolded_SA, truth)
-        else:
-            chi2_SA = compute_chi2(unfolded_SA, truth, cov_SA)
 
         # Hybrid solver
         unfolded_HYB, error_HYB, chi2_HYB = None, None, None
-        if quantum == True:
-            unfolded_HYB, error_HYB, cov_HYB, _ = unfolder.solve_hybrid_sampler(
-                num_toys=num_toys
+        if hybrid == True:
+            unfolded_HYB, error_HYB, chi2_HYB = run_QUnfold(
+                "Hyb", unfolder, response, measured, truth, num_reads, num_toys
             )
-            chi2_HYB = None
-            if cov_HYB is None:
-                chi2_HYB = compute_chi2(unfolded_HYB, truth)
-            else:
-                chi2_HYB = compute_chi2(unfolded_HYB, truth, cov_HYB)
 
-        # Make RooUnfold response
+        ################################### RooUnfold ###################################
+
         m_response = ROOT.RooUnfoldResponse(h_mc_measured, h_mc_truth, m_response)
         m_response.UseOverflow(True)
 
         # Unfold with RooUnfold IBU
-        unfolder = ROOT.RooUnfoldBayes("IBU", "Iterative Bayesian")
-        unfolder.SetIterations(4)
-        unfolder.SetVerbose(0)
-        unfolder.SetSmoothing(0)
-        unfolder.SetResponse(m_response)
-        unfolder.SetMeasured(h_measured)
-        unfolded_IBU_histo = None
-        if num_toys == 1:
-            unfolded_IBU_histo = unfolder.Hunfold(unfolder.kErrors)
-        elif num_toys > 1:
-            unfolder.SetNToys(num_toys)
-            unfolded_IBU_histo = unfolder.Hunfold(unfolder.kCovToys)
-        unfolded_IBU = TH1_to_array(unfolded_IBU_histo)
-        start, stop = 0, unfolded_IBU_histo.GetNbinsX() + 2
-        error_IBU = np.array(
-            [unfolded_IBU_histo.GetBinError(i) for i in range(start, stop)]
+        unfolded_IBU, error_IBU, chi2_IBU = run_RooUnfold(
+            "IBU", m_response, h_measured, truth, num_toys
         )
-        chi2_IBU = None
-        if num_toys == 1:
-            chi2_IBU = compute_chi2(unfolded_IBU, truth)
-        elif num_toys > 1:
-            cov_IBU = unfolder.Eunfold(unfolder.kCovToys)
-            chi2_IBU = compute_chi2(unfolded_IBU, truth, TMatrix_to_array(cov_IBU))
 
-        # Unfold with RooUnfold Matrix Inversion
-        unfolder = ROOT.RooUnfoldInvert("MI", "Matrix Inversion")
-        unfolder.SetVerbose(0)
-        unfolder.SetResponse(m_response)
-        unfolder.SetMeasured(h_measured)
-        unfolded_MI_histo = None
-        if num_toys == 1:
-            unfolded_MI_histo = unfolder.Hunfold(unfolder.kErrors)
-        elif num_toys > 1:
-            unfolder.SetNToys(num_toys)
-            unfolded_MI_histo = unfolder.Hunfold(unfolder.kCovToys)
-        unfolded_MI = TH1_to_array(unfolded_MI_histo)
-        start, stop = 0, unfolded_MI_histo.GetNbinsX() + 2
-        error_MI = np.array(
-            [unfolded_MI_histo.GetBinError(i) for i in range(start, stop)]
+        # Unfold with RooUnfold MI
+        unfolded_MI, error_MI, chi2_MI = run_RooUnfold(
+            "MI", m_response, h_measured, truth, num_toys
         )
-        chi2_MI = None
-        if num_toys == 1:
-            chi2_MI = compute_chi2(unfolded_MI, truth)
-        elif num_toys > 1:
-            cov_MI = unfolder.Eunfold(unfolder.kCovToys)
-            chi2_MI = compute_chi2(unfolded_MI, truth, TMatrix_to_array(cov_MI))
+
+        ################################### Results ###################################
 
         # SA results
         SA_info = {
@@ -320,7 +337,7 @@ def make_comparisons(reco, particle):
 
         # HYB results
         HYB_info = None
-        if quantum == True:
+        if hybrid == True:
             HYB_info = {
                 "mean": unfolded_HYB,
                 "std": error_HYB,
@@ -351,5 +368,5 @@ def make_comparisons(reco, particle):
             measured,
             binning,
             var,
-            quantum,
+            hybrid,
         )
