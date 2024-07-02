36875367242e43994d6cf8971196f7fdbfd18ece
SimoneGasperini <simone.gasperini4@unibo.it>
2024-07-02 17:45:11 +0200

    Rearrange requirements files and fix setup script

--- src/setup.py
+++ src/setup.py
@@ -1,20 +1,25 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    with open("./requirements.txt", "r") as file:
+        reqs = file.read().splitlines()
+    return reqs
+
 
 if __name__ == "__main__":
+
     setup(
         name="QUnfold",
-        packages=find_packages(),
         version="0.3.0",
         author="Gianluca Bianco, Simone Gasperini",
         author_email="biancogianluca9@gmail.com, simone.gasperini4@unibo.it",
         url="https://github.com/JustWhit3/QUnfold/tree/main/src/QUnfold",
         python_requires=">=3.8",
-        install_requires=[
-            "numpy",
-            "scipy",
-            "matplotlib",
-            "dwave-ocean-sdk",
-            "gurobipy",
-            "tqdm",
-        ],
+        packages=find_packages(),
+        install_requires=get_requirements(),
+        license="MIT",
+        license_files="LICENSE",
+        long_description_content_type="text/markdown",
+        long_description="README.md",
     )
