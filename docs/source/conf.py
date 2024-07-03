339630390a9d9c0d3b34b8dbbc493d64f44ddd57
SimoneGasperini <simone.gasperini4@unibo.it>
2024-07-03 18:08:25 +0200

    Update package version for new release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "QUnfold"
 copyright = "2024, Gianluca Bianco, Simone Gasperini"
 author = "Gianluca Bianco, Simone Gasperini"
-release = "0.3.0"  # version
+release = "0.3.1"  # version
 master_doc = "index"
 
 
