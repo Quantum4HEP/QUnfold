506cd1bfa60c6b519e370cd7c1d71c2ff979d720
DrWatt <marco.lorusso96@gmail.com>
2024-07-05 10:53:21 +0200

    First attempt at lambda optimizer using scipy

--- src/QUnfold/utils.py
+++ src/QUnfold/utils.py
@@ -31,3 +31,20 @@ def compute_chi2(observed, expected, covariance):
     chi2 = residuals.T @ np.linalg.pinv(covariance) @ residuals
     chi2_red = chi2 / len(expected)
     return chi2_red
+    
+
+from QUnfold import QUnfoldQUBO
+from QUnfold.utils import compute_chi2
+from scipy.optimize import minimize_scalar
+from scipy.optimize import minimize_scalar
+
+def lambda_optimizer(response, measured, truth, maxlam = 1, minlam = 0):
+    def lamba_func(lam = .1):
+        unfolder = QUnfoldQUBO(response, measured, lam=lam)
+        unfolder.initialize_qubo_model()
+        sol, err, cov = unfolder.solve_gurobi_integer()
+        chi2 = compute_chi2(observed=sol, expected=truth, covariance=cov)
+        return chi2
+
+    minimizer = minimize_scalar(lamba_func,bracket=(minlam,maxlam),method='brent' ,options={'disp':3})
+    return minimizer.x
