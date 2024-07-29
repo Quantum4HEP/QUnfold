6aa81fb08e5745c6cea293c19ed5d70f8c115a0f
SimoneGasperini <simone.gasperini4@unibo.it>
2024-07-29 22:42:23 +0200

    Set global plotting configurations and update the QUnfoldPlotter class

--- src/QUnfold/QUnfoldPlotter.py
+++ src/QUnfold/QUnfoldPlotter.py
@@ -3,27 +3,96 @@ import pylab as plt
 from QUnfold.utils import compute_chi2
 
 
-class QUnfoldPlotter:
-    """
-    Class used to plot QUnfold data and results.
-    """
+############ PLOTTING CONFIGURATIONS ############
+label2color = {
+    "Truth": "tab:blue",
+    "Measured": "tab:orange",
+    "MI": "tab:green",  # matrix inversion
+    "IBU": "tab:red",  # iterative bayesian
+    "SA": "black",  # simulated annealing
+    "HYB": "tab:purple",  # hybrid solver
+    "QA": "tab:olive",  # quantum annealing
+    "GRB": "tab:pink",  # gurobi integer
+}
+figsize = (12, 9)
+chi2_ndigits = 2
+alpha = 0.2
+marker = "o"
+markersize = 3
+ticks_fontsize = 10
+labels_fontsize = 15
+legend_fontsize = 12
+#################################################
 
+
+def histogram_plot(ax, x, y, label):
+    y = np.append(y, [y[-1]])
+    color = label2color[label]
+    ax.step(x, y, label=label, color=color, where="post")
+    ax.fill_between(x, y, color=color, alpha=alpha, step="post")
+
+
+def errorbar_plot(ax, x, y, yerr, method, chi2, binning):
+    rounded_chi2 = round(chi2, ndigits=chi2_ndigits)
+    label = rf"{method} ($\chi^2 = {rounded_chi2}$)"
+    color = label2color[method]
+    ax.errorbar(
+        x=x,
+        y=y,
+        yerr=yerr,
+        label=label,
+        color=color,
+        marker=marker,
+        ms=markersize,
+        linestyle="None",
+    )
+    ax.set_xlim(left=binning[0], right=binning[-1])
+    ax.tick_params(
+        reset=True,
+        direction="inout",
+        labelsize=ticks_fontsize,
+        top=False,
+        right=False,
+        labelbottom=False,
+    )
+    ax.set_ylabel(ylabel="Entries", fontsize=labels_fontsize)
+    ax.set_ylim(bottom=0)
+    ax.legend(fontsize=legend_fontsize)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+
+
+def ratio_plot(ax, x, y, yerr, method, binning):
+    ax.axhline(y=1, color=label2color["Truth"])
+    color = label2color[method]
+    ax.errorbar(
+        x=x,
+        y=y,
+        yerr=yerr,
+        color=color,
+        marker=marker,
+        ms=markersize,
+        linestyle="None",
+    )
+    ax.tick_params(
+        reset=True,
+        direction="inout",
+        labelsize=ticks_fontsize,
+        right=False,
+    )
+    ax.set_xticks(np.linspace(binning[0], binning[-1], num=len(binning)))
+    ax.set_yticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75])
+    ax.set_yticklabels(["", "0.5", "", "1.0", "", "1.5", ""])
+    ax.set_xlabel(xlabel="Bins", fontsize=labels_fontsize)
+    ax.set_ylabel(ylabel="Ratio to\nTruth", fontsize=labels_fontsize)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+
+
+class QUnfoldPlotter:
     def __init__(
         self, response, measured, truth, unfolded, error, covariance, binning, chi2=True
     ):
-        """
-        Constructs a QUnfoldPlotter class for visualizing unfolding results.
-
-        Args:
-            response (numpy.ndarray): response matrix used in the unfolding process.
-            measured (numpy.ndarray): measured data histogram.
-            truth (numpy.ndarray): truth histogram.
-            unfolded (numpy.ndarray): unfolded histogram.
-            error (numpy.ndarray): errors on the unfolded histogram.
-            covariance (numpy.array): covariance matrix of the unfolding.
-            binning (numpy.ndarray): binning of the histograms.
-            chi2 (bool): if True, it shows the chi2 on the plot.
-        """
         self.response = response[1:-1, 1:-1]
         self.measured = measured[1:-1]
         self.truth = truth[1:-1]
@@ -33,13 +102,9 @@ class QUnfoldPlotter:
         self.binning = binning[1:-1]
         self.chi2 = chi2
 
-    def _plotResponseSetup(self):
-        """
-        Set up the response matrix plot for drawing or saving: the response matrix is set up as a heatmap,
-        with the column representing the measured variable and the row representing the truth variable.
-        """
-        # Set up plot
-        plt.imshow(
+    def _plot_response(self):
+        fig, ax = plt.subplots(figsize=figsize)
+        ax.imshow(
             np.transpose(self.response),
             cmap="viridis",
             extent=[
@@ -50,122 +115,67 @@ class QUnfoldPlotter:
             ],
             origin="lower",
         )
-        plt.colorbar()
-        plt.xlabel("Truth")
-        plt.ylabel("Measured")
+        ax.tick_params(
+            reset=True,
+            direction="inout",
+            labelsize=ticks_fontsize,
+            top=False,
+            right=False,
+        )
+        ax.set_xlabel("Truth", fontsize=labels_fontsize)
+        ax.set_ylabel("Measured", fontsize=labels_fontsize)
+        fig.tight_layout()
 
-    def plotResponse(self):
-        """
-        Plot the response matrix with matplotlib style.
-        """
-        self._plotResponseSetup()
-        plt.show()
-        plt.close()
-
-    def saveResponse(self, path):
-        """
-        Save the plotted response matrix with matplotlib style to a file.
-
-        Args:
-            path (str): file path to save the plot.
-        """
-        self._plotResponseSetup()
-        plt.savefig(path)
-        plt.close()
-
-    def _plotSetup(self, method):
-        """
-        Create an histogram comparison among measured, truth and unfolded distributions,
-        with the chi2 among unfolded and truth distribution.
-
-        Args:
-            method (str): unfolding method.
-        """
-        # Divide into subplots
-        fig = plt.figure()
-        gs = fig.add_gridspec(2, 1, height_ratios=[3, 1], hspace=0)
+    def _plot_histograms(self, method):
+        fig = plt.figure(figsize=figsize)
+        gs = fig.add_gridspec(nrows=2, ncols=1, height_ratios=[3, 1], hspace=0)
         ax1 = fig.add_subplot(gs[0])
         ax2 = fig.add_subplot(gs[1], sharex=ax1)
 
-        # Plot truth
-        truth_steps = np.append(self.truth, [self.truth[-1]])
-        ax1.step(
-            self.binning, truth_steps, label="Truth", where="post", color="tab:blue"
-        )
-        ax1.fill_between(
-            self.binning, truth_steps, step="post", alpha=0.3, color="tab:blue"
-        )
+        histogram_plot(ax=ax1, x=self.binning, y=self.truth, label="Truth")
+        histogram_plot(ax=ax1, x=self.binning, y=self.measured, label="Measured")
 
-        # Plot measured
-        meas_steps = np.append(self.measured, [self.measured[-1]])
-        ax1.step(
-            self.binning, meas_steps, label="Measured", where="post", color="tab:orange"
-        )
-
-        # Plot unfolded histogram with chi2 test
-        binwidths = np.diff(self.binning)
-        bin_midpoints = self.binning[:-1] + binwidths / 2
-        chi2 = round(compute_chi2(self.unfolded, self.truth, self.covariance), 2)
-        label = rf"Unfolded {method} ($\chi^2 = {chi2}$)"
-        ax1.errorbar(
+        binning = self.binning
+        bin_midpoints = binning[:-1] + np.diff(binning) / 2
+        chi2 = compute_chi2(self.unfolded, self.truth)
+        errorbar_plot(
+            ax=ax1,
             x=bin_midpoints,
             y=self.unfolded,
             yerr=self.error,
-            label=label,
-            marker="o",
-            ms=3.5,
-            c="green",
-            linestyle="None",
+            method=method,
+            chi2=chi2,
+            binning=binning,
         )
 
-        # Plot ratio QUnfold to truth
-        ax2.axhline(y=1, color="tab:blue")
-        ax2.errorbar(
+        ratio_sol = self.unfolded / self.truth
+        ratio_err = self.error / self.truth
+        ratio_plot(
+            ax=ax2,
             x=bin_midpoints,
-            y=self.unfolded / self.truth,
-            yerr=self.error / self.truth,
-            ms=3.5,
-            fmt="o",
-            color="g",
+            y=ratio_sol,
+            yerr=ratio_err,
+            method=method,
+            binning=binning,
         )
+        fig.tight_layout()
 
-        # Plot style settings
-        ax1.tick_params(axis="x", which="both", bottom=True, top=False, direction="in")
-        ax2.tick_params(axis="x", which="both", bottom=True, top=True, direction="in")
-        ax1.set_xlim(self.binning[0], self.binning[-1])
-        ax1.set_ylim(0, ax1.get_ylim()[1])
-        ax2.set_yticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75])
-        ax2.set_yticklabels(["", "0.5", "", "1.0", "", "1.5", ""])
-        ax1.tick_params(
-            axis="x", which="both", bottom=False, top=False, labelbottom=False
-        )
-
-        # Plot settings
-        ax2.set_ylabel("Ratio to\ntruth")
-        ax2.set_xlabel("Bins")
-        ax1.set_ylabel("Entries")
-        ax1.legend(loc="upper right")
-        plt.tight_layout()
-
-    def plot(self, method=""):
-        """
-        Plot the measured distribution histogram.
-
-        Args:
-            method (str): unfolding method.
-        """
-        self._plotSetup(method)
+    def show_response(self):
+        self._plot_response()
         plt.show()
         plt.close()
 
-    def savePlot(self, path, method=""):
-        """
-        Save the plot of the measured distribution histogram into path.
+    def save_response(self, path):
+        self._plot_response()
+        plt.savefig(path)
+        plt.close()
 
-        Args:
-            path (str): file path to save the plot.
-            method (str): unfolding method.
-        """
-        self._plotSetup(method)
+    def show_histograms(self, method):
+        self._plot_histograms(method)
+        plt.show()
+        plt.close()
+
+    def save_histograms(self, path, method):
+        self._plot_histograms(method)
         plt.savefig(path)
         plt.close()
