573537077fc751241fcbd662a1521ca2dc6bb30d
SimoneGasperini <simone.gasperini4@unibo.it>
2024-08-03 16:09:41 +0200

    Fix QPlotter class to include plotting functions as static methods

--- src/qunfold/qplotter.py
+++ src/qunfold/qplotter.py
@@ -3,99 +3,40 @@ import pylab as plt
 from qunfold.utils import compute_chi2
 
 
-############ PLOTTING CONFIGURATIONS ############
+################### PLOTTING CONFIGURATIONS ###################
+# https://matplotlib.org/stable/gallery/color/named_colors.html
 label2color = {
     "Truth": "tab:blue",
     "Measured": "tab:orange",
-    "MI": "tab:green",  # matrix inversion
-    "IBU": "tab:red",  # iterative bayesian
-    "SA": "black",  # simulated annealing
-    "HYB": "tab:purple",  # hybrid solver
-    "QA": "tab:olive",  # quantum annealing
-    "GRB": "tab:pink",  # gurobi integer
+    "MI": "purple",  # matrix inversion
+    "IBU": "red",  # iterative bayesian
+    "SA": "green",  # simulated annealing
+    "HYB": "dodgerblue",  # hybrid solver
+    "QA": "orchid",  # quantum annealing
 }
+default_color = "black"
 figsize = (12, 9)
 chi2_ndigits = 2
 alpha = 0.25
 marker = "o"
 markersize = 4
-ticks_fontsize = 9
-labels_fontsize = 15
-legend_fontsize = 12
-#################################################
-
-
-def histogram_plot(ax, x, y, label):
-    y = np.append(y, [y[-1]])
-    color = label2color[label]
-    ax.step(x, y, label=label, color=color, where="post")
-    ax.fill_between(x, y, color=color, alpha=alpha, step="post")
-
-
-def errorbar_plot(ax, x, y, yerr, binning, method, chi2):
-    rchi2 = round(chi2, ndigits=chi2_ndigits)
-    label = rf"Unfolded {method} ($\chi^2 = {rchi2}$)"
-    color = label2color[method]
-    ax.errorbar(
-        x=x,
-        y=y,
-        yerr=yerr,
-        label=label,
-        color=color,
-        marker=marker,
-        ms=markersize,
-        linestyle="None",
-    )
-    ax.set_xlim(left=binning[0], right=binning[-1])
-    ax.tick_params(
-        reset=True,
-        direction="inout",
-        labelsize=ticks_fontsize,
-        top=False,
-        right=False,
-        labelbottom=False,
-    )
-    yticks = [-np.inf, -np.inf] + ax.get_yticks().tolist()[2:]
-    yticklabels = ["", ""] + ax.get_yticklabels()[2:]
-    ax.set_yticks(yticks)
-    ax.set_yticklabels(yticklabels)
-    ax.set_ylabel(ylabel="Entries", fontsize=labels_fontsize)
-    ax.set_ylim(bottom=0)
-    ax.legend(fontsize=legend_fontsize)
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-
-
-def ratio_plot(ax, x, y, yerr, binning, method, xlabel=None):
-    if xlabel is None:
-        xlabel = "Bins"
-    ax.axhline(y=1, color=label2color["Truth"])
-    color = label2color[method]
-    ax.errorbar(
-        x=x,
-        y=y,
-        yerr=yerr,
-        color=color,
-        marker=marker,
-        ms=markersize,
-        linestyle="None",
-    )
-    ax.tick_params(
-        reset=True,
-        direction="inout",
-        labelsize=ticks_fontsize,
-        right=False,
-    )
-    ax.set_xticks(binning)
-    ax.set_xlabel(xlabel=xlabel, fontsize=labels_fontsize)
-    ax.set_ylabel(ylabel="Ratio to\nTruth", fontsize=labels_fontsize)
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
+ticks_fontsize = 8
+labels_fontsize = 14
+legend_fontsize = 10
+###############################################################
 
 
 class QPlotter:
     def __init__(
-        self, response, measured, truth, unfolded, covariance, binning, method
+        self,
+        response,
+        measured,
+        truth,
+        unfolded,
+        covariance,
+        binning,
+        method,
+        normed=True,
     ):
         self.response = response[1:-1, 1:-1]
         self.measured = measured[1:-1]
@@ -103,27 +44,82 @@ class QPlotter:
         self.unfolded = unfolded[1:-1]
         self.covariance = covariance[1:-1, 1:-1]
         self.binning = binning[1:-1]
-        self.qunfold_method = method
+        self.method = method
+        self.normed = normed
 
-    def _plot_response(self):
-        fig, ax = plt.subplots(figsize=figsize)
-        heatmap = ax.imshow(
-            np.transpose(self.response),
-            extent=[
-                self.binning[0],
-                self.binning[-1],
-                self.binning[0],
-                self.binning[-1],
-            ],
-            origin="lower",
+    @staticmethod
+    def histogram_plot(ax, xedges, hist, label, ylabel, norm=None):
+        if norm is not None:
+            hist = hist / norm
+        hist = np.append(hist, [hist[-1]])
+        color = label2color.get(label, default_color)
+        ax.step(xedges, hist, label=label, color=color, where="post")
+        ax.fill_between(xedges, hist, color=color, alpha=alpha, step="post")
+        ax.set_ylabel(ylabel, fontsize=labels_fontsize)
+
+    @staticmethod
+    def errorbar_plot(ax, xmid, hist, err, xlims, label, chi2, norm=None):
+        if norm is not None:
+            hist = hist / norm
+            err = err / norm
+        color = label2color.get(label, default_color)
+        rchi2 = round(chi2, ndigits=chi2_ndigits)
+        label = rf"Unfolded {label} ($\chi^2 = {rchi2}$)"
+        ax.errorbar(
+            x=xmid,
+            y=hist,
+            yerr=err,
+            label=label,
+            color=color,
+            marker=marker,
+            ms=markersize,
+            linestyle="None",
         )
         ax.tick_params(
-            reset=True,
-            direction="inout",
             labelsize=ticks_fontsize,
             top=False,
             right=False,
+            labelbottom=False,
+            reset=True,
         )
+        yticks = [-np.inf, -np.inf] + ax.get_yticks().tolist()[2:]
+        yticklabels = ["", ""] + ax.get_yticklabels()[2:]
+        ax.set_yticks(yticks)
+        ax.set_yticklabels(yticklabels)
+        ax.legend(fontsize=legend_fontsize)
+        ax.set_xlim(left=xlims[0], right=xlims[1])
+        ax.set_ylim(bottom=0)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+
+    @staticmethod
+    def ratio_plot(ax, xmid, ratio, err, label, xticks, xlabel):
+        ax.axhline(y=1, color=label2color["Truth"])
+        color = label2color.get(label, default_color)
+        ax.errorbar(
+            x=xmid,
+            y=ratio,
+            yerr=err,
+            color=color,
+            marker=marker,
+            ms=markersize,
+            linestyle="None",
+        )
+        ax.tick_params(labelsize=ticks_fontsize, right=False, reset=True)
+        ax.set_xticks(xticks)
+        ax.set_xlabel(xlabel, fontsize=labels_fontsize)
+        ax.set_ylabel("Ratio to\nTruth", fontsize=labels_fontsize)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+
+    def _plot_response(self):
+        fig, ax = plt.subplots(figsize=figsize)
+        xs = ys = self.binning
+        R = self.response
+        heatmap = ax.pcolormesh(xs, ys, R.T)
+        ax.set_xticks(xs)
+        ax.set_yticks(ys)
+        ax.tick_params(labelsize=ticks_fontsize, reset=True)
         ax.set_xlabel("Measured", fontsize=labels_fontsize)
         ax.set_ylabel("Truth", fontsize=labels_fontsize)
         fig.colorbar(heatmap, ax=ax)
@@ -135,22 +131,53 @@ class QPlotter:
         ax1 = fig.add_subplot(gs[0])
         ax2 = fig.add_subplot(gs[1], sharex=ax1)
 
-        histogram_plot(ax=ax1, x=self.binning, y=self.truth, label="Truth")
-        histogram_plot(ax=ax1, x=self.binning, y=self.measured, label="Measured")
-
-        binning = self.binning
-        method = self.qunfold_method
-        xmid = binning[:-1] + np.diff(binning) / 2
-        obs, exp, cov = self.unfolded, self.truth, self.covariance
+        measured, truth = self.measured, self.truth
+        sol, cov = self.unfolded, self.covariance
         err = np.sqrt(np.diag(cov))
-        chi2 = compute_chi2(obs, exp, cov)
-        errorbar_plot(
-            ax=ax1, x=xmid, y=obs, yerr=err, method=method, binning=binning, chi2=chi2
+        sol_ratio = sol / truth
+        err_ratio = err / truth
+        label = self.method
+        widths = np.diff(self.binning)
+        xmid = self.binning[:-1] + 0.5 * widths
+        xlims = (self.binning[0], self.binning[-1])
+        ylabel = "Frequency" if self.normed else "Entries"
+        chi2 = compute_chi2(sol, truth, covariance=cov)
+        norm = np.sum(truth) if self.normed else None
+
+        self.histogram_plot(
+            ax=ax1,
+            xedges=self.binning,
+            hist=truth,
+            label="Truth",
+            ylabel=ylabel,
+            norm=norm,
         )
-        ratio_sol = obs / self.truth
-        ratio_err = err / self.truth
-        ratio_plot(
-            ax=ax2, x=xmid, y=ratio_sol, yerr=ratio_err, method=method, binning=binning
+        self.histogram_plot(
+            ax=ax1,
+            xedges=self.binning,
+            hist=measured,
+            label="Measured",
+            ylabel=ylabel,
+            norm=norm,
+        )
+        self.errorbar_plot(
+            ax=ax1,
+            xmid=xmid,
+            hist=sol,
+            err=err,
+            xlims=xlims,
+            label=label,
+            chi2=chi2,
+            norm=norm,
+        )
+        self.ratio_plot(
+            ax=ax2,
+            xmid=xmid,
+            ratio=sol_ratio,
+            err=err_ratio,
+            label=label,
+            xticks=self.binning,
+            xlabel="Bins",
         )
         fig.tight_layout()
 
