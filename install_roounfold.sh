e5e135b709165494eee4eef763ae51b1b1a52e33
SimoneGasperini <simone.gasperini4@unibo.it>
2024-08-16 16:58:02 +0200

    Fix ROOT and RooUnfold installation script

--- install_roounfold.sh
+++ install_roounfold.sh
@@ -6,8 +6,9 @@ conda config --set channel_priority strict
 conda install -c conda-forge root -y
 
 # Install RooUnfold from source using pip
+conda install gcc_linux-64 gxx_linux-64
 pip install git+https://gitlab.cern.ch/RooUnfold/RooUnfold
 
 # Clear conda environment
-conda clean --all -y
+conda clean -a -y
 pip cache purge
